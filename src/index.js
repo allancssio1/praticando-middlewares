c1abf082a8dc1be20ac7ec79e47e8210f429f51b
Allan Cassio <allan.cassio@oqvocequer.com.br>
2022-04-06 16:57:41 -0300

    concluido desafio de middleware

--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) return response.status(404).json({error: 'Usuário não cadastrado!'})
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if(!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({error: 'Você já superou os limites de sua conta.'})
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+  const {id} = request.params
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({error: "usuário não exoncontrado"})
+  }
+
+  if(!validate(id)){
+    return response.status(400).json({error: "id não é valido"})
+  }
+
+  const todoSelected = user.todos.find(todo => todo.id === id)
+
+  if(!todoSelected) {
+    return response.status(404).json({error: "todo não é encontrado"})
+  }
+
+  request.todo = todoSelected
+  request.user = user
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if(!user) return response.status(404).json({error: "usuário não exoncontrado"})
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
